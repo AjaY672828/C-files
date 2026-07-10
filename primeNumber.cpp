5e8e1e6a9adfde96ba51bfd9c3375a77a67555a1
Ajay <rawatajay5304@gmail.com>
2026-07-10 13:06:13 +0530

    Fixed prime number logic

---
+++ primeNumber.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int n = 15;
+    bool isPrime = true;
+    for (i = 2; i <= n-1; i++)
+    {
+        if (n%i==0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+    if(isPrime==true){
+        cout<< "This is a prime number." << endl;
+    }
+    else{
+        cout<< "This is not a prime number." << endl;
+    }
+    return 0;
+}
