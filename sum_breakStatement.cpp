95eebcaa2b6e287b2e2b5029c7e15d83af384da4
Ajay <rawatajay5304@gmail.com>
2026-07-13 02:33:23 +0530

    resolve merge conflicts in c++ pattern files

--- sum_breakStatement.cpp
+++ sum_breakStatement.cpp
@@ -18,8 +18,5 @@ int main()
     }
     cout << "Sum of n numbers is: " << sum << endl;
     return 0;
-<<<<<<< HEAD:sum.cpp
+
 }
-=======
-}   
->>>>>>> 2e2204d3d1b8e2cd48b5e8dca667a9e05ed6f533:sum_breakStatement.cpp
